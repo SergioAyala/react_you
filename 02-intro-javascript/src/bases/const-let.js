be4d5c28848b9aacd3928808045285b249175261
Sergio Esteban Ayala Del Castillo <sergio.ayala@junaeb.cl>
2025-10-15 15:32:12 -0300

    clases

---
+++ 02-intro-javascript/src/bases/const-let.js
@@ -0,0 +1,18 @@
+
+
+
+const nombre = 'sergio';
+let apellido = 'ayala';
+
+let valorDado = 5;
+valorDado = 4;
+
+
+if ( true )
+    {
+        const edad = 31;
+        let valorDado = 6;
+        console.log(edad);
+    }
+
+console.log(nombre,apellido,valorDado);

cadb85cc2b291744018467b90521d0ea0510ba81
Sergio Esteban Ayala Del Castillo <sergio.ayala@junaeb.cl>
2025-10-20 16:22:44 -0300

    seguir en clases 10 endpoint

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,3 +1,13 @@
 
 
 const apiKey = 'YQkNsMB4dL2j8j6jLNlZ0bKJ0mNPabyk';
+
+const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+
+
+peticion.then( resp => {
+    resp.json().then(data => {
+    console.log(data);
+    })
+}).catch(console.warn);
+

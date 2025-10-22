92b26262e67940a9a9eec584c82b9fc5ecae75f8
Sergio Esteban Ayala Del Castillo <sergio.ayala@junaeb.cl>
2025-10-22 15:17:26 -0300

    clase 2 vite

---
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,14 @@
+/* function HelloWorldApp() {
+    return (<h1>hola!!!</h1>);
+}
+
+export default HelloWorldApp;  */
+
+import React from 'react'
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>Hello World App</h1>
+  )
+}
+ 
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-function App() {
-    return (<h1>hola!!!</h1>);
-}
+import{ HelloWorldApp} from '../src/HelloWorldApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App></App>
+        <HelloWorldApp></HelloWorldApp>
     </React.StrictMode>
 )

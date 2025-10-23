53c148b98a1586977128314f943a70894f9552a5
Sergio Esteban Ayala Del Castillo <sergio.ayala@junaeb.cl>
2025-10-23 11:35:51 -0300

    clase 6 vite

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import{ HelloWorldApp} from '../src/HelloWorldApp';
 import{ FirstApp} from '../src/FirstApp';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
